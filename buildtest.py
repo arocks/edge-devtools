a828d98a3c469627ffec8ee7082443185f959c58
Arun Ravindran <arunvr@gmail.com>
2021-04-10 11:08:12 +0530

    Cleaned up build test and removed username for django3 branch

--- buildtest.py
+++ buildtest.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 import argparse
-
-
-parser = argparse.ArgumentParser()
-# parser.add_argument("template", help="the template file to templatify")
-# parser.add_argument("-o", "--output", dest="outfile", help="Output file")
-args = parser.parse_args()
-
 import os
 import shutil
 from subprocess import call
@@ -16,55 +9,68 @@ SUPERUSER = "a"
 SUPERUSER_PASSWORD = "a"
 SUPERUSER_EMAIL = "a@a.com"
 
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 if os.path.isdir(PROJNAME):
     print("Removing existing directory...")
     shutil.rmtree(PROJNAME)
 
-call(["django-admin.py",
-      "startproject",
-      "--verbosity=3",
-      "--template=../edge",
-      "--extension=py,md,html,env",
-      PROJNAME])
+call(
+    [
+        "django-admin.py",
+        "startproject",
+        "--verbosity=3",
+        "--template=../edge",
+        "--extension=py,md,html,env",
+        PROJNAME,
+    ]
+)
 print("Project build complete...")
 
 import sys
-src_dir = os.path.join(BASE_DIR, PROJNAME, 'src')
+
+src_dir = os.path.join(BASE_DIR, PROJNAME, "src")
 sys.path.insert(0, src_dir)
 os.chdir(src_dir)
 
 print("Copying local.env from sample...")
-shutil.copyfile("{0}/settings/local.sample.env".format(PROJNAME),
-                "{0}/settings/local.env".format(PROJNAME))
+shutil.copyfile(
+    "{0}/settings/local.sample.env".format(PROJNAME),
+    "{0}/settings/local.env".format(PROJNAME),
+)
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "{0}.settings.development".format(PROJNAME))
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "{0}.settings.development".format(PROJNAME)
+)
 
 import django
+
 django.setup()
 print("Django configured...")
 
 from django.core import management
+
 print("Checking apps...")
-management.call_command('check')
+management.call_command("check")
 print("Migrating...")
-management.call_command('migrate', interactive=False, verbosity=2)
+management.call_command("migrate", interactive=False, verbosity=2)
 
 # Create superuser
 print("Creating superuser...")
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
-User.objects.create_superuser(email=SUPERUSER_EMAIL, name="Abc",
-                              password=SUPERUSER_PASSWORD)
+User.objects.create_superuser(email=SUPERUSER_EMAIL, password=SUPERUSER_PASSWORD)
 
 # Run tests
-print("Running tests...")
-management.call_command('test', 'profiles', verbosity=0)
+# print("Running tests...")
+# management.call_command('test', 'profiles', verbosity=0)
 
 # Track changes
-os.chdir("..")                  # Up one level from `src` back to proj root
+os.chdir("..")  # Up one level from `src` back to proj root
 print("Setting a git repo...")
 shutil.copyfile("../_gitignore_my_proj", ".gitignore")
 call(["git", "init"])
